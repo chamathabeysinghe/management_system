d3bd26350917d4c43002d201623981bd9803a196
Chamath Abeysinghe <Chamath Abeysinghe>
2016-05-01 15:54:08 +0530

    items

---
+++ laravel/database/migrations/2016_05_01_101301_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('serial_number');
+            $table->string('item_name');
+            $table->integer('unit_cost');
+            $table->integer('sale_type');
+            $table->integer('owner_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('items');
+    }
+}
