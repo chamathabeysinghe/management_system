76b95e5864fc40db9ba6a2022ac2c8d8bcec8ea9
Thilina Ashen Gamage <ashen.workplace@gmail.com>
2016-05-11 13:58:26 +0530

    updating create quotation, quotation summary and master.blade files

--- laravel/resources/views/quotation_management/create_quotation.blade.php
+++ laravel/resources/views/quotation_management/create_quotation.blade.php
@@ -47,66 +47,62 @@
                     <label class="active" for="tel">Client Phone</label>
                 </div>
             </div>
+
+            <div class="row">
+                <div id="table" class="table-editable">
+                    <span class="table-add glyphicon glyphicon-plus"></span>
+                    <table class="table highlight bordered">
+                        <thead>
+                        <tr>
+                            <th data-field="item_code">Item Code</th>
+                            <th data-field="item">Item</th>
+                            <th data-field="description">Description</th>
+                            <th class="right-align" data-field="unit_price">Unit Price</th>
+                            <th class="right-align" data-field="quantity">Quantity</th>
+                            <th class="right-align" data-field="total_price">Total Cost</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <!-- This is our clonable table line -->
+                        <tr class="hide">
+                            <td contenteditable="true">DESXXXX</td>
+                            <td contenteditable="true">unknown item</td>
+                            <td contenteditable="true">unknown</td>
+                            <td class="right-align" contenteditable="true">$$</td>
+                            <td class="right-align" contenteditable="true">unknown quantity</td>
+                            <td class="right-align" contenteditable="true">$$$$</td>
+                            <td>
+                                <span class="table-remove glyphicon glyphicon-remove"></span>
+                            </td>
+                            <td>
+                                <span class="table-up glyphicon glyphicon-arrow-up"></span>
+                                <span class="table-down glyphicon glyphicon-arrow-down"></span>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <button  id="export-btn" class="btn btn-primary">Export Data</button>
+                <p id="export"></p>
+            </div>
+
+
             <input type="hidden" name="_token" value="{{Session::token()}}">
             <button type="submit" class="btn waves-effect waves-light" type="submit" name="action">Create
                 <i class="material-icons right">done</i>
             </button>
+
+            <button class="btn waves-effect waves-light" type="submit" name="action">Download
+                <i class="material-icons right">play_for_work</i>
+            </button>
+            <button class="btn waves-effect waves-light" type="submit" name="action">Print
+                <i class="material-icons right">print</i>
+            </button>
+            <button class="btn waves-effect waves-light" type="submit" name="action">Email
+                <i class="material-icons right">email</i>
+            </button>
+
         </form>
-    </div>
-
-    <div class="row">
-        <div id="table" class="table-editable">
-            <span class="table-add glyphicon glyphicon-plus"></span>
-            <table class="table highlight bordered">
-                <thead>
-                    <tr>
-                        <th data-field="item Code">Item Code</th>
-                        <th data-field="item">Item</th>
-                        <th data-field="description">Description</th>
-                        <th class="right-align" data-field="unit_price">Unit Price</th>
-                        <th class="right-align" data-field="quantity">Quantity</th>
-                        <th class="right-align" data-field="total_price">Total Cost</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <!-- This is our clonable table line -->
-                <tr class="hide">
-                    <td contenteditable="true">DESXXXX</td>
-                    <td contenteditable="true">unknown item</td>
-                    <td contenteditable="true">unknown</td>
-                    <td class="right-align" contenteditable="true">$$</td>
-                    <td class="right-align" contenteditable="true">unknown quantity</td>
-                    <td class="right-align" contenteditable="true">$$$$</td>
-                    <td>
-                        <span class="table-remove glyphicon glyphicon-remove"></span>
-                    </td>
-                    <td>
-                        <span class="table-up glyphicon glyphicon-arrow-up"></span>
-                        <span class="table-down glyphicon glyphicon-arrow-down"></span>
-                    </td>
-                </tr>
-            </table>
-        </div>
-
-        <button  id="export-btn" class="btn btn-primary">Export Data</button>
-
-
-
-        <p id="export"></p>
-    </div>
-
-    <div class="row">
-
-        <button class="btn waves-effect waves-light" type="submit" name="action">Download
-            <i class="material-icons right">play_for_work</i>
-        </button>
-        <button class="btn waves-effect waves-light" type="submit" name="action">Print
-            <i class="material-icons right">print</i>
-        </button>
-        <button class="btn waves-effect waves-light" type="submit" name="action">Email
-            <i class="material-icons right">email</i>
-        </button>
     </div>
 
 @endsection
