067ed08e02bf1671892b60b6eef6ba0a8f6a5ce3
Thilina Ashen Gamage <ashen.workplace@gmail.com>
2016-05-17 18:15:01 +0530

    Search boxes removed from Quotation and Estimation Summary pages

--- laravel/resources/views/quotation_management/estimation_summary.blade.php
+++ laravel/resources/views/quotation_management/estimation_summary.blade.php
@@ -14,23 +14,6 @@
     <section class="row" xmlns="http://www.w3.org/1999/html">
         <div class="col-lg-8 col-lg-offset-2">
             <header style="color: #747474;"><h3>Estimation Dashboard</h3></header>
-
-            {{--<div>--}}
-            {{--<input type="text" id="search-input" placeholder="Search" onkeydown="down()" onkeyup="up()">--}}
-            {{--</div>--}}
-
-
-            <nav>
-                <div class="nav-wrapper teal lighten-2">
-                    <form>
-                        <div class="input-field">
-                            <input id="search-input" type="search" required onkeydown="down()" onkeyup="up()" style="height: 65px;">
-                            <label for="search"><i class="material-icons">search</i></label>
-                            <i class="material-icons">close</i>
-                        </div>
-                    </form>
-                </div>
-            </nav>
         </div>
     </section>
 
